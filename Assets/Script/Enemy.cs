4bd3e87eae02ab2929dcd5f407e9e142cf9a59b1
Thélonious Guibert <168654169+Thelo2410@users.noreply.github.com>
2025-05-27 16:58:01 +0200

    Fixe HealthBarEnemy

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -57,6 +57,7 @@ public class Enemy : MonoBehaviour
             StopCoroutine(flashRoutine);
 
         flashRoutine = StartCoroutine(FlashRed());
+        StartCoroutine(Shake());
 
         if (currentHealth <= 0)
         {
@@ -72,6 +73,23 @@ public class Enemy : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
 
         spriteRenderer.color = originalColor;
+    }
+
+    IEnumerator Shake(float duration = 0.15f, float magnitude = 0.05f)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float offsetX = Random.Range(-1f, 1f) * magnitude;
+            float offsetY = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = originalPos + new Vector3(offsetX, offsetY, 0);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = originalPos;
     }
 
 
